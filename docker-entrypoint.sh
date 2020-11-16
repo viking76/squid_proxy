8aafd3d511686b9aeaca182bba79d6574c2b2e94
Gaël <liaudetgael@gmail.com>
2020-11-16 18:38:17 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i 's/EN/'${LANG}'/g' /apps/squid.conf.cache
-sed -i 's/3128/'${PORT}'/g' /apps/squid.conf.cache
+sed -i 's/EN/'${LANG}'/g' /apps/squid.conf.intercept
+sed -i 's/3128/'${PORT}'/g' /apps/squid.conf.intercept
 
 #if [[ "${USE_SSL:-0}" == "1" ]] ; then
 #    sed -i 's/ssl: false/ssl: true/g' /usr/src/app/web/vue/dist/UIconfig.js
 #fi
 
-exec /apps/squid/sbin/squid -N -f /apps/squid.conf.cache -z
+exec /apps/squid/sbin/squid -N -f /apps/squid.conf.intercept -z
